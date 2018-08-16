acc6d030e3d8e2cab4bc220aa54d2817e0888629
Anton Arapov <arapov@gmail.com>
2018-08-16 16:42:53 +0200

    roster: apiGetGroups 1/n
    Signed-off-by: Anton Arapov <arapov@gmail.com>

--- controller/roster/roster.go
+++ controller/roster/roster.go
@@ -1,16 +1,63 @@
 package roster
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
+	"time"
 
 	"github.com/arapov/pile2/lib/flight"
+	"github.com/arapov/pile2/model/roster"
 
 	"github.com/blue-jay-fork/core/router"
 )
 
+// Must obey http://jsonapi.org/format/
+type response struct {
+	Data   []map[string][]string `json:"data"`
+	Errors []map[string]string   `json:"errors"`
+	Meta   map[string]string     `json:"meta"`
+}
+
 // Load the routes.
 func Load() {
 	router.Get("/roster", Index)
+
+	router.Get("/api/v1/roster/groups", apiGetGroups)
+}
+
+func apiGetGroups(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/vnd.api+json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	c := flight.Context(w, r)
+	start := time.Now()
+
+	var res response
+	var data []map[string][]string
+
+	data, err := roster.GetGroups(c.LDAP)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusBadGateway)
+		res.Errors = append(res.Errors, map[string]string{"title": err.Error()})
+
+		goto out
+	}
+	res.Data = data
+
+out:
+	elapsed := time.Since(start)
+	res.Meta = map[string]string{"time": elapsed.String()}
+	jsonRes, err := json.Marshal(res)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		res.Errors = append(res.Errors, map[string]string{"title": err.Error()})
+
+		goto out
+	}
+
+	w.Write(jsonRes)
 }
 
 // Index displays the page.
--- model/roster/roster.go
+++ model/roster/roster.go
@@ -1,1 +1,17 @@
 package roster
+
+import (
+	"log"
+)
+
+type Connection interface {
+	Query(filter string, attributes []string) ([]map[string][]string, error)
+}
+
+func GetGroups(ldapc Connection) ([]map[string][]string, error) {
+	log.Println("getting groups...")
+
+	// TODO: c.Query()
+
+	return nil, nil
+}
