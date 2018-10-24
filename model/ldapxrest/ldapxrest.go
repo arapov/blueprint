bf5487df8b238e76dc076faea6336f50c9b39c24
Anton Arapov <arapov@gmail.com>
2018-10-24 15:01:11 +0200

    rework query api parser
    Signed-off-by: Anton Arapov <arapov@gmail.com>

--- model/ldapxrest/ldapxrest.go
+++ model/ldapxrest/ldapxrest.go
@@ -16,26 +16,54 @@ func Query(ldapc Connection, formValues map[string][]string) ([]map[string][]str
 	var filter string
 	var attributes []string
 
-	attributes = nil
-	filter = "(&"
-	for key, values := range formValues {
+	var andKeyList = make(map[string][]string)
+	var orKeyList = make(map[string][]string)
+	for key, formValue := range formValues {
+		var values []string
+		for _, value := range formValue {
+			values = append(values, strings.Split(value, ",")...)
+		}
+
 		if key == "attributes" {
-			attributes = strings.Split(values[0], ",")
+			attributes = values
 			continue
 		}
 
-		value := strings.Split(values[0], ",")
-		if len(value) > 1 {
-			filter += "(|"
-			for _, v := range value {
-				filter += fmt.Sprintf("(%s=%s)", key, v)
+		for _, value := range values {
+			if key[0] == byte('|') {
+				cleanKey := strings.TrimLeft(key, "|")
+				orKeyList[cleanKey] = append(orKeyList[cleanKey], value)
+			} else {
+				andKeyList[key] = append(andKeyList[key], value)
 			}
-			filter += ")"
-		} else {
-			filter += fmt.Sprintf("(%s=%s)", key, values[0])
 		}
 	}
-	filter += ")"
+
+	var andFilter string
+	for k, values := range andKeyList {
+		for _, value := range values {
+			andFilter += fmt.Sprintf("(%s=%s)", k, value)
+		}
+	}
+	if andFilter != "" {
+		filter = fmt.Sprintf("(&%s)", andFilter)
+	}
+
+	var orFilter string
+	for k, values := range orKeyList {
+		for _, value := range values {
+			if andFilter == "" {
+				orFilter += fmt.Sprintf("(%s=%s)", k, value)
+			} else if len(orKeyList)+len(values) == 2 {
+				orFilter += fmt.Sprintf("(&%s)(%s=%s)", andFilter, k, value)
+			} else {
+				orFilter += fmt.Sprintf("(&%s(%s=%s))", andFilter, k, value)
+			}
+		}
+	}
+	if orFilter != "" {
+		filter = fmt.Sprintf("(|%s)", orFilter)
+	}
 
 	res, err := ldapc.Query(filter, attributes)
 	if err != nil {
