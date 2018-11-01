5549e37bfdc62d53b1bc81e236a152a4240340b1
Anton Arapov <arapov@gmail.com>
2018-11-01 14:37:04 +0100

    add dropdown to the group cards
    Signed-off-by: Anton Arapov <arapov@gmail.com>

--- model/roster/roster.go
+++ model/roster/roster.go
@@ -10,14 +10,18 @@ type Connection interface {
 }
 
 func GetGroups(ldapc Connection, group ...string) ([]map[string][]string, error) {
-	prefix := os.Getenv("LDAP_GROUPS_PREFIX") // TODO: Find a better, generic place
+	// TODO: Find a better, generic place:
+	groupPrefix := os.Getenv("LDAP_GROUPS_PREFIX")
+	subGroupPrefix := os.Getenv("LDAP_SUBGROUPS_PREFIX")
+
+	// TODO: Generalize. Handles just one specific group atm.
 	if group[0] != "" {
-		prefix = group[0]
+		groupPrefix = group[0]
 	}
 
 	// objectClass rhatRoverGroup hardcoded due to app specific case, it's
-	// unlikely to be used anywhere else
-	filter := fmt.Sprintf("(&(objectClass=rhatRoverGroup)(cn=%s*))", prefix)
+	// unlikely and not meant to be used anywhere else
+	filter := fmt.Sprintf("(&(objectClass=rhatRoverGroup)(cn=%s*)(!cn=*%s*))", groupPrefix, subGroupPrefix)
 	attributes := []string{"cn", "description"}
 
 	return ldapc.Query(filter, attributes)
